57e745e59040f81af8161b58b2cad3ef6f9b55ed
Matteo Valenzi <dilloawallace@hotmail.com>
2025-11-12 18:02:15 +0100

    .

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,43 @@
-let s: string = "pippo";
-console.log(s);
+import express, { NextFunction, Request, Response } from "express";
+import { config } from "dotenv";
+config({ quiet: true });
 
-let frutti = ["mela", "pera", "banana"]
+const port = process.env.PORT ?? 3000;
+const app = express();
 
-for (const frutto of frutti) {
-    console.log(frutto);
-}
+
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log("LOG:", req.method, req.url);
+    next();
+});
+
+app.get("/", (req: Request, res: Response) => {
+    res.json({ messaggio: "Benvenuto" });
+});
+
+app.get("/ciao", (req: Request, res: Response) => {
+    res.send(`<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <title>Ciao</title>
+                </head>
+                <body>
+                    <h1>Ciao!</h1>
+                </body>
+                </html>
+                `);
+});
+
+
+// 404 sottointeso
+
+
+// 500 sottointeso
+
+app.listen(port, () => {
+    console.log(`Server in esecuzione su http://localhost:${port}`);
+    console.log("Premere CTRL+C per arrestare");
+});
